974701af9021a46e427e90c5dc54d5100ce30cd7
smsaifurrahman <smsaifur525@gmail.com>
2024-04-26 16:27:46 +0200

    Private Routing is completed

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CountryCard from './CountryCard';
 
 const Countries = () => {
+    const [countries,setCountries] = useState([])
+
+    fetch('http://localhost:5000/countries')
+    .then(res =>res.json())
+    .then(data => {
+        setCountries(data)
+    })
+
     return (
         <div>
-            Countries
+            Countries {countries.length}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    countries.map(country => <CountryCard country={country} key={country._id}></CountryCard>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Components/CountryCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CountryCard = ({country}) => {
+    const {image, countryName, description }= country || {}
+    return (
+        <div className="card card-compact  bg-base-100 shadow-xl">
+  <figure><img src={image} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{countryName}</h2>
+    <p>{description.slice(0,100)}</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default CountryCard;
